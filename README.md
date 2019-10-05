8846acf484bb167144c67f9b3cd1291a37dc8137
chrisyandev <czyanhome@hotmail.com>
2019-10-05 01:47:45 -0700

    Finished project

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # interactive-form
+
+This project is for Treehouse's Full-Stack Techdegree program.
+
+This program uses jQuery extensively to add customized behavior and interactivity. It uses regular expressions to validate
+user input, and provides helpful error messages for invalid inputs. A personal goal was to make it as robust as possible,
+so that the program will still work when more data is entered.
+
+### Project Goals
+
+To meet the following expectations:
+
+* jQuery
+  * Project includes jQuery and utilizes it in at least some aspect of the form.
+
+* Focus on the first field
+  * On page load, the cursor appears in the "Name" field, ready for a user to type.
+
+* Job Role Section
+  * "Your job role" text field appears when user selects "Other" from the Job Role menu.
+
+* T-Shirt Section
+  * Until a theme is selected from the “Design” menu, no color options appear in the “Color” drop down and the “Color” field reads “Please select a T-shirt theme”.
+  * When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.
+
+* Activity Registration
+  * User cannot select two activities that are at the same time.
+  * Total cost of selected activities is calculated and displayed below the list of activities.
+
+* Displaying payment sections
+  * The "Credit Card" payment option is selected by default.
+  * Payment option in the select menu matches the payment option displayed on the page.
+  * When a user chooses a payment option, the chosen payment section is revealed and the other payment sections are hidden.
+
+* Form Validation
+  * Form cannot be submitted (the page does not refresh when the submit button is clicked) until the following requirements have been met:
+    * Name field isn’t blank.
+    * Email field contains validly formatted e-mail address: (doesn’t have to check that it's a real e-mail address, just that it's formatted like one).
+    * At least one checkbox under "Register for Activities" section must be selected.
+    * If "Credit Card" is the selected payment option, the three fields accept only numbers: a 13 to 16-digit credit card number, a 5-digit zip code, and 3-number CVV value.
+
+* Form Validation Messages
+  * On submission, the form provides an error indication or message for each field that requires validation:
+    * Name field
+    * Email field
+    * “Register for Activities” checkboxes
+    * Credit Card number, Zip code, and CVV, only if the credit card payment method is selected.
+
+* Form works without JavaScript
+  * When JavaScript is disabled, all form fields and payment information is displayed, including the "Other" field under the "Job Role" section.
---
+++ js/script.js
@@ -0,0 +1,327 @@
+const $otherJobField = $('#other-title');
+const $jobTitleSelect = $('#title');
+
+const $tshirtColorSelect = $('#color');
+const $tshirtColorOptions = $('#color option');
+const $tshirtDesignSelect = $('#design');
+const JSPunsColors = [
+	'cornflowerblue',
+	'darkslategrey',
+	'gold',
+];
+const iHeartJSColors = [
+	'tomato',
+	'steelblue',
+	'dimgrey'
+];
+
+const $activitiesCollection = $('fieldset.activities input[type="checkbox"]');
+const checkedActivities = [];
+let runningTotal = 0;
+
+const $paymentSelect = $('#payment');
+const $creditCardDiv = $('#credit-card');
+const $paypalDiv = $('#paypal');
+const $bitcoinDiv = $('#bitcoin');
+
+const $submitButton = $('button[type="submit"]');
+
+/*** Start of on page load section ***/
+$('#name').focus();
+
+$otherJobField.hide();
+
+$tshirtColorSelect.prepend('<option value="please select tshirt">Please select a T-shirt theme</option>');
+$tshirtColorSelect.val('please select tshirt');
+$tshirtColorOptions.hide();
+
+$paymentSelect.val('Credit Card');
+$('#payment [value="select method"]').remove();
+$paypalDiv.hide();
+$bitcoinDiv.hide();
+/** End of on page load section **/
+
+// Shows the input field for 'Other' Job Role
+$jobTitleSelect.on('change', () => {
+	if ($jobTitleSelect.val() === 'other') {
+		$otherJobField.show();
+		$otherJobField.focus();
+	} else {
+		$otherJobField.hide();
+	}
+});
+
+$tshirtDesignSelect.on('change', () => {
+	$tshirtColorOptions.hide();
+
+	// Shows the color options that match the colors in 'colorArray' argument
+	function showAvailableColors(colorArray) {
+		$.each($tshirtColorOptions, (indexA, valueA) => {
+			$.each(colorArray, (indexB, valueB) => {
+				if (valueA.getAttribute('value') === valueB) {
+					$tshirtColorOptions.eq(indexA).show();
+				}
+			});
+		});
+	}
+
+	const pleaseSelectTshirtOption = $('#color [value="please select tshirt"]');
+	// Updates available options
+	if ($tshirtDesignSelect.val() === 'js puns') {
+		showAvailableColors(JSPunsColors);
+		$tshirtColorSelect.val(JSPunsColors[0]);
+		pleaseSelectTshirtOption.hide();
+	}
+	if ($tshirtDesignSelect.val() === 'heart js') {
+		showAvailableColors(iHeartJSColors);
+		$tshirtColorSelect.val(iHeartJSColors[0]);
+		pleaseSelectTshirtOption.hide();
+	}
+	if ($('#design option:selected').text().toLowerCase() === 'select theme') {
+		pleaseSelectTshirtOption.show();
+		$tshirtColorSelect.val('please select tshirt');
+	}
+});
+
+$activitiesCollection.on('click', (e) => {
+	// Finds a string with format T00:00:00- which denotes the start time
+	function findStartTime(string) {
+		const startTimeRegex = /T(0[0-9]|1[0-9]|2[0-3]|[0-9]):([0-5][0-9]):([0-5][0-9])-/;
+		if (startTimeRegex.test(string)) {
+			return string.match(startTimeRegex);
+		}
+		return null;
+	}
+
+	// Finds a string with format -T00:00:00 which denotes the end time
+	function findEndTime(string) {
+		const endTimeRegex = /-T(0[0-9]|1[0-9]|2[0-3]|[0-9]):([0-5][0-9]):([0-5][0-9])$/;
+		if (endTimeRegex.test(string)) {
+			return string.match(endTimeRegex);
+		}
+		return null;
+	}
+
+	function toMilliseconds(hour, minute, second) {
+		return hour * (1000 * 60 * 60) + minute * (1000 * 60) + second * (1000);
+	}
+
+	// Returns the start and end time in milliseconds
+	function findAndConvertTime(checkbox) {
+		const $dayAndTime = checkbox.attr('data-day-and-time');
+		const startTime = findStartTime($dayAndTime);
+		const endTime = findEndTime($dayAndTime);
+		if (startTime === null || endTime === null) {
+			return null;
+		}
+		const startTimeInMs = toMilliseconds(startTime[1], startTime[2], startTime[3]);
+		const endTimeInMs = toMilliseconds(endTime[1], endTime[2], endTime[3]);
+		const obj = {
+			start: startTimeInMs,
+			end: endTimeInMs
+		};
+		return obj;
+	}
+
+	/* Tests if a time range overlaps with another time range. Takes into account
+	situations where a range's start and end are inside another range (eg. 2-4 and 1-5).
+	Start times are allowed to overlap with end times. */
+	function timeIsAvailable(a_Start, a_End, b_Start, b_End) {
+		if (a_Start >= b_Start && a_Start < b_End) {
+			return false;
+		}
+		if (a_End > b_Start && a_End <= b_End) {
+			return false;
+		}
+		if (b_Start >= a_Start && b_Start < a_End) {
+			return false;
+		}
+		if (b_End > a_Start && b_End <= a_End) {
+			return false;
+		}
+		return true;
+	}
+
+	const $elementTarget = $(e.target);
+	const targetTime = findAndConvertTime($elementTarget);
+	const $targetName = $elementTarget.attr('name');
+	// Adds to running total, appends it, and adds an object associated with e.target
+	if ($elementTarget.is(':checked')) {
+		const costInInteger = parseInt($elementTarget.attr('data-cost').match(/\d+/));
+		runningTotal += costInInteger;
+
+		checkedActivities.push({
+			name: $targetName,
+			disabled: []
+		});
+
+		// Appends if it's the first checkbox checked, else updates appended value.
+		if (checkedActivities.length === 1) {
+			$('fieldset.activities').append('<span id="running-total">Total: $' + runningTotal + '</span>');
+		} else {
+			$('#running-total').html('Total: $' + runningTotal);
+		}
+	}
+
+	// If we are checking a checkbox, and if this checkbox has a time, loop through all checkboxes
+	if ($elementTarget.is(':checked') && targetTime !== null) {
+		for (let i = 0; i < $activitiesCollection.length; i++) {
+			const $ithCheckbox = $activitiesCollection.eq(i);
+			const ithCheckboxTime = findAndConvertTime($ithCheckbox);
+			/* Skips checked checkboxes and ones that don't have a time. 
+			Adds to the 'disabled' property of object associated with e.target. */
+			if (!($ithCheckbox.is(':checked')) && ithCheckboxTime !== null) {
+				if (!timeIsAvailable(ithCheckboxTime.start, ithCheckboxTime.end, targetTime.start, targetTime.end)) {
+					$ithCheckbox.attr('disabled', true);
+					const checkedActivity = checkedActivities.find((obj) => {
+						return obj.name === $targetName;
+					});
+					checkedActivity.disabled.push($ithCheckbox.attr('name'));
+				}
+			}
+		}
+	// If we are unchecking the checkbox. Get the disabled checkboxes associated with e.target, and enable them.
+	} else if (!($elementTarget.is(':checked')) && targetTime !== null) {
+		const checkedActivity = checkedActivities.find((obj) => {
+			return obj.name === $targetName;
+		});
+		for (const x of checkedActivity.disabled) {
+			const $checkbox = $('.activities [name="' + x + '"]');
+			$checkbox.attr('disabled', false);
+		}
+	}
+
+	// Subtracts from running total and removes object associated with e.target
+	if (!($elementTarget.is(':checked'))) {
+		const costInInteger = parseInt($elementTarget.attr('data-cost').match(/\d+/));
+		runningTotal -= costInInteger;
+
+		const checkedActivityIndex = checkedActivities.findIndex((obj) => {
+			return obj.name === $targetName;
+		});
+		checkedActivities.splice(checkedActivityIndex, 1);
+
+		// Removes running total if no checkboxes checked, else updates appended value.
+		if (checkedActivities.length === 0) {
+			$('#running-total').remove();
+		} else {
+			$('#running-total').html('Total: $' + runningTotal);
+		}
+	}
+});
+
+$paymentSelect.on('change', () => {
+	if ($paymentSelect.val() === 'Credit Card') {
+		$creditCardDiv.show();
+		$paypalDiv.hide();
+		$bitcoinDiv.hide();
+	}
+	if ($paymentSelect.val() === 'PayPal') {
+		$paypalDiv.show();
+		$creditCardDiv.hide();
+		$bitcoinDiv.hide();
+	}
+	if ($paymentSelect.val() === 'Bitcoin') {
+		$bitcoinDiv.show();
+		$creditCardDiv.hide();
+		$paypalDiv.hide();
+	}
+});
+
+$submitButton.on('click', (e) => {
+	let isNameValid = false;
+	let isEmailValid = false;
+	let isAnyActivityChecked = false;
+	let isCardNumberValid = false;
+	let isZipCodeValid = false;
+	let isCvvValid = false;
+
+	const $nameField = $('#name');
+	const $emailField = $('#mail');
+	const $cardNumberField = $('#cc-num');
+	const $zipCodeField = $('#zip');
+	const $cvvField = $('#cvv');
+
+	const nameRegex = /^[a-z]+ ?[a-z]*$/i;
+	const emailRegex = /^[^@]+@[^@.]+\.[a-z]+\.?[a-z]*$/i;
+	const creditCardRegex = /^[0-9]{13,16}$/;
+	const zipCodeRegex = /^[0-9]{5}$/;
+	const cvvRegex = /^[0-9]{3}$/;
+
+	// Resets error messages because some fields may be valid when user submits again
+	$('#name-error').remove();
+	$('#email-error').remove();
+	$('#activities-error').remove();
+	$('#card-number-error').remove();
+	$('#zip-code-error').remove();
+	$('#cvv-error').remove();
+
+	/* The following 'if' conditions tests if user input satisfies certain conditions.
+	If a particular input is not valid, an error message is appended if it does not
+	exist already. */
+	if (nameRegex.test($nameField.val())) {
+		isNameValid = true;
+	} else {
+		$nameField.prop('style', 'border:2px solid red');
+		if ($('#name-error').length < 1) {
+			$nameField.after('<div id="name-error" style="color:red;padding-bottom:15px">Invalid Name</div>');
+		}
+	}
+
+	if (emailRegex.test($emailField.val())) {
+		isEmailValid = true;
+	} else {
+		$emailField.prop('style', 'border:2px solid red');
+		if ($('#email-error').length < 1) {
+			$emailField.after('<div id="email-error" style="color:red;padding-bottom:15px">Invalid Email</div>');
+		}
+	}
+
+	if (checkedActivities.length > 0) {
+		isAnyActivityChecked = true;
+	} else {
+		if ($('#activities-error').length < 1) {
+			const $lastLabel = $activitiesCollection.eq($activitiesCollection.length - 1).parent();
+			$lastLabel.after(`<div id="activities-error" style="color:red;padding-bottom:15px">
+								Must select at least one activity</div>`);
+		}
+	}
+
+	if ($paymentSelect.val() === 'Credit Card') {
+		if (creditCardRegex.test($cardNumberField.val())) {
+			isCardNumberValid = true;
+			console.log('cn valid');
+		} else {
+			$cardNumberField.prop('style', 'border:2px solid red');
+			if ($('#card-number-error').length < 1) {
+				$cardNumberField.after('<div id="card-number-error" style="color:red;padding-bottom:15px">Invalid Card Number</div>');
+			}
+		}
+
+		if (zipCodeRegex.test($zipCodeField.val())) {
+			isZipCodeValid = true;
+			console.log('zipcode valid');
+		} else {
+			$zipCodeField.prop('style', 'border:2px solid red');
+			if ($('#zip-code-error').length < 1) {
+				$zipCodeField.after('<div id="zip-code-error" style="color:red;padding-bottom:15px">Invalid Zip Code</div>');
+			}
+		}
+
+		if (cvvRegex.test($cvvField.val())) {
+			isCvvValid = true;
+			console.log('cvv valid');
+		} else {
+			$cvvField.prop('style', 'border:2px solid red');
+			if ($('#cvv-error').length < 1) {
+				$cvvField.after('<div id="cvv-error" style="color:red;padding-bottom:15px">Invalid CVV</div>');
+			}
+		}
+	}
+
+	// If any field is invalid, don't submit
+	if (isNameValid === false || isEmailValid === false || isAnyActivityChecked === false
+		|| isCardNumberValid === false || isZipCodeValid === false || isCvvValid === false) {
+		e.preventDefault();
+	}
+});
